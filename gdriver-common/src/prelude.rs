f08c9937e49c929ab3a3d850f40a94d7d8ba57a1
OMGeeky <aalaalgmx@gmail.com>
2024-02-18 11:32:16 +0100

    move around prelude stuff

--- gdriver-common/src/prelude.rs
+++ gdriver-common/src/prelude.rs
@@ -1,5 +1,10 @@
 pub use crate::config::Configuration;
 pub use crate::config::CONFIGURATION;
+pub use crate::drive_structure::drive_id::{DriveId, ROOT_ID};
 pub use crate::ipc;
-pub(crate) type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-pub(crate) use std::result::Result as StdResult;
+pub mod result {
+    pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+    pub use std::result::Result as StdResult;
+}
+pub(crate) use result::*;
