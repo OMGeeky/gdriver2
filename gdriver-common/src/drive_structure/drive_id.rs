8a8b638bfb236a0fd8c6e70cdca670df46ebe4b9
OMGeeky <aalaalgmx@gmail.com>
2024-02-18 11:33:34 +0100

    move into submodule

---
+++ gdriver-common/src/drive_structure/drive_id.rs
@@ -0,0 +1,29 @@
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
+
+lazy_static! {
+    pub static ref ROOT_ID: DriveId = DriveId(String::from("root"));
+}
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct DriveId(pub String);
+
+impl<T> From<T> for DriveId
+where
+    T: Into<String>,
+{
+    fn from(s: T) -> Self {
+        DriveId(s.into())
+    }
+}
+
+impl AsRef<str> for DriveId {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+impl Display for DriveId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]", self.0)
+    }
+}
